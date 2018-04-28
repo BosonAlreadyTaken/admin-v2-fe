2a72187c1600c671b342905d36f12463042823c2
Bosn <bosn@Bosns-iMac.local>
2018-04-28 10:18:40 +0800

    通用部分的开发

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
 
-import 'font-awesome/css/font-awesome.min.css';
-import './app.css';
-import './index.scss';
+//页面
+import Home from 'page/home/index.jsx';
+import Layout from 'component/layout/index.jsx';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <Router>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/product" component={Home} />
+                        <Route path="/product-category" component={Home} />
+                    </Switch>
+                </Layout>
+            </Router>
+        )
+    }
+}
 ReactDOM.render(
-    <div>
-        <i className='fa fa-address-book'></i>
-        <h1>Hello, world</h1>
-    </div>,
+    <App/>,
     document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,12 @@ module.exports = {
         publicPath: "/dist/",
         filename: 'js/app.js'
     },
+    resolve: {
+        alias: {
+            page: path.resolve(__dirname, './src/page'),
+            component: path.resolve(__dirname, './src/component')
+        }
+    },
     module: {
       rules: [
           {
@@ -67,7 +73,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            favicon: './favicon.ico'
         }),
         //独立css文件
         new ExtractTextPlugin('css/[name].css'),
@@ -77,6 +84,9 @@ module.exports = {
         })
     ],
     devServer: {
-        port: 8086
+        port: 8086,
+        historyApiFallback: {
+            index: '/dist/index.html'
+        }
     }
 };
